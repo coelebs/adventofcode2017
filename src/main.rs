68700cbf1b770ef8d50304f7ae4d8bbee6886d6b
Vincent Kriek <vincent@kriek.org>
2017-12-02 20:59:54 +0100

    Updated day1 with file as input

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,12 @@ const USAGE: &'static str = "
 Advent of Code.
 
 Usage:
-    advent_of_code day1 <captcha>
+    advent_of_code day1 <path>
     advent_of_code day2 <path>
 ";
 
 #[derive(Debug, Deserialize)]
 struct Args {
-    arg_captcha: String,
     arg_path: String,
     cmd_day1: bool,
     cmd_day2: bool,
@@ -29,8 +28,8 @@ fn main() {
                             .unwrap_or_else(|e| e.exit());
 
     if args.cmd_day1 {
-        let solution1 = day1::solve_part1(&args.arg_captcha);
-        let solution2 = day1::solve_part2(&args.arg_captcha);
+        let solution1 = day1::solve_part1_file(&args.arg_path).ok().unwrap();
+        let solution2 = day1::solve_part2_file(&args.arg_path).ok().unwrap();
         println!("Solution part 1: {:?}\nSolution part 2: {:?}", solution1, solution2);
     }
 
